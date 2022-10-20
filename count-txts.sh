3f71b1adc617f1b55962ff738a6e05feb0c27189
Juliet Raynovich <jraynovi@ucsd.edu>
2022-10-20 16:43:50 -0700

    Update count-txts.sh

--- count-txts.sh
+++ count-txts.sh
@@ -1,6 +1,3 @@
-find docsearch/technical/
-find technica/ > find-results.txt
-less find-results.txt
-wc find-results.txt
+find docsearch/technica/ > find-results.txt
 grep ".txt" find-results.txt > grep-results.txt
 wc grep-results.txt
